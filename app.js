1f905a9fdc21f6c6099a7bf2c9d6936ce39fef57
bronwyndet <wayward@waywardcoffee.com>
2016-07-19 13:48:28 -0700

    Corrected syntax of toLowerCase method

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var userName = prompt("What is your name?")
+"use strict"
+
+var userName = prompt('What is your name?');
 
 console.log('Greetings, ' + userName + '! It \'s nice to meet you. Let\'s answer some questions about me! Please answer with yes or no, or y or n');
 
-var answerOne = prompt("Does Bronwyn ride a motorcycle?");
+var answerOne = prompt('Does Bronwyn ride a motorcycle?');
 
-answerOne.toLowerCase();
 
-if (answerOne = 'yes' || answerOne = 'y') {
-    alert('That\'s right! Bronwyn does ride a motorcycle!')
+if (answerOne.toLowerCase() === 'yes' || answerOne.toLowerCase() === 'y') {
+    console.log('That\'s right! Bronwyn does ride a motorcycle!')
 } else {
-    alert('You must not have noticed the boots she\'s wearing today! She does ride a motorcycle!');
-}
+    console.log('You must not have noticed the boots she\'s wearing today! She does ride a motorcycle!');
+};
