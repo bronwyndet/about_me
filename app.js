f53e1bcd72ad9b2ea414d595d0cc4b2b0554d903
bronwyndet <wayward@waywardcoffee.com>
2016-07-19 13:07:59 -0700

    First question added to JS file.

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-alert('Greetings all!');
+var userName = prompt("What is your name?")
+
+console.log('Greetings, ' + userName + '! It \'s nice to meet you. Let\'s answer some questions about me! Please answer with yes or no, or y or n');
+
+var answerOne = prompt("Does Bronwyn ride a motorcycle?");
+
+answerOne.toLowerCase();
+
+if (answerOne = 'yes' || answerOne = 'y') {
+    alert('That\'s right! Bronwyn does ride a motorcycle!')
+} else {
+    alert('You must not have noticed the boots she\'s wearing today! She does ride a motorcycle!');
+}
