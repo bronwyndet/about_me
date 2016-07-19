7c9c21673e5966fb115bcf95b5b97680d062c39a
bronwyndet <wayward@waywardcoffee.com>
2016-07-19 14:14:24 -0700

    Adding third question to JS.

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ console.log('Greetings, ' + userName + '! It \'s nice to meet you. Let\'s answer
 var answerOne = prompt('Does Bronwyn ride a motorcycle?');
 
 if (answerOne.toLowerCase() === 'yes' || answerOne.toLowerCase() === 'y') {
-    console.log('That\'s right! Bronwyn does ride a motorcycle!')
+    document.write('That\'s right! Bronwyn does ride a motorcycle!')
 } else {
-    console.log('You must not have noticed the boots she\'s wearing today! She does ride a motorcycle!');
+    document.write('You must not have noticed the boots she\'s wearing today! She does ride a motorcycle!');
 };
 
 var answerTwo = prompt('Does Bronwyn have a daughter?');
@@ -20,12 +20,12 @@ if (answerTwo.toLowerCase() === 'yes' || answerTwo.toLowerCase() === 'y') {
     console.log('You haven\'t been paying attention! She does have a daughter.');
 };
 
-var answerThree = prompt('');
+var answerThree = prompt('Is Bronwyn a huge fan of Firefly/Serenity?');
 
 if (answerThree.toLowerCase() === 'yes' || answerThree.toLowerCase() === 'y') {
-    console.log('');
+    console.log('So you noticed the t-shirts! Yes, she is a Browncoat!');
 } else {
-    console.log('');
+    console.log('Sorry, you got that one wrong. She\'s a huge fan of Firefly and Serenity');
 };
 
 var answerFour = prompt('');
